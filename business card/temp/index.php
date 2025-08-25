6e5d799ba16025d7fc1591c910a5011b49d55aec
Jannynguyen <120248978+Jannynguyen@users.noreply.github.com>
2025-08-25 17:19:49 +0700

    Add files via upload

---
+++ business card/temp/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Business Card</title>
+    <link href="styles.css" rel="stylesheet"/>
+</head>
+
+<body>
+<div class="business-card">
+    <img class="profile-image" src="https://cdn.freecodecamp.org/curriculum/labs/flower.jpg" alt ="profile-image"></img>
+<p class="full-name">Janny Nguyen</p>
+<p class="designation">Operator</p>
+<p class="company">Pega</p>
+<hr>
+<p class="email">Janny_nguyen@pegatron.corp.com</p>
+<p class="phone">0996766554</p>
+<a href="#">Portfolio</a>
+<hr>
+<div class="social-media">
+    <h2>Connect with me</h2>
+    <a href="https://www.freecodecamp.org/learn">Twitter</a>
+    <a href="https://www.freecodecamp.org/learn">LinkedIn</a>
+    <a href="https://www.freecodecamp.org/learn">GitHub</a>
+</div></div>
+</body>
+
+</html>
